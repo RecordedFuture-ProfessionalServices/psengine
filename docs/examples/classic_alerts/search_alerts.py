dddd4f2c89fa3e2de10657facb055d1b85baac5b
mmedici <moise.medici@recordedfuture.com>
2025-08-22 08:34:27 +0100

    classic alerts doc

---
+++ docs/examples/classic_alerts/search_alerts.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+
+from psengine.classic_alerts import ClassicAlertMgr
+from psengine.classic_alerts.constants import ALL_CA_FIELDS
+
+OUTPUT_DIR = Path(__file__).parent / 'alerts'
+OUTPUT_DIR.mkdir(exist_ok=True)
+
+mgr = ClassicAlertMgr()
+alerts = mgr.search(triggered='-1d', status='New', fields=ALL_CA_FIELDS)
+
+for alert in alerts:
+    markdown = alert.markdown(ai_insights=False, triggered_by=False, defang_iocs=True)
+    (OUTPUT_DIR / f'{alert.id_}.md').write_text(markdown)
