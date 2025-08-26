3616972a5aaf81e14aa1ca271f77f0eabc691384
mmedici <moise.medici@recordedfuture.com>
2025-08-26 11:34:36 +0100

    enrichment

---
+++ docs/examples/enrich/example_2.py
@@ -0,0 +1,9 @@
+from psengine.enrich import LookupMgr
+
+URLS = ['http://www.example.com/1', 'http://www.example.com/2']
+mgr = LookupMgr()
+
+urls = mgr.lookup_bulk(URLS, 'url', fields=['links'], max_workers=2)
+for url in urls:
+    if url.is_enriched:
+        print(url)
---
+++ docs/examples/enrich/example_3.py
@@ -0,0 +1,26 @@
+import csv
+from pathlib import Path
+
+from psengine.enrich import SoarMgr
+
+OUTPUT_DIR = Path(__file__).parent / 'enrich'
+OUTPUT_DIR.mkdir(exist_ok=True)
+
+to_enrich_file = OUTPUT_DIR / 'to_enrich.csv'
+to_enrich_file.write_text('ip\n1.1.1.1\n2.2.2.2')
+
+enriched_file = OUTPUT_DIR / 'enriched.csv'
+
+mgr = SoarMgr()
+
+with to_enrich_file.open(newline='') as f:
+    reader = csv.DictReader(f)
+    ips_to_enrich = [row['ip'] for row in reader]
+
+enriched_ips = mgr.soar(ip=ips_to_enrich)
+
+with enriched_file.open('w', newline='') as f:
+    writer = csv.writer(f)
+    writer.writerow(['ip', 'score'])
+    for ip, enriched in zip(ips_to_enrich, enriched_ips):
+        writer.writerow([ip, enriched.content.risk.score])
