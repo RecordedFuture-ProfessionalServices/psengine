71e2ed61a69515415a713e4e07600bfbeefbdec8
mmedici <moise.medici@recordedfuture.com>
2025-09-04 15:27:46 +0100

    [ci skip] example config fix

--- docs/examples/config/example_1.py
+++ docs/examples/config/example_1.py
@@ -1,5 +1,8 @@
+from pathlib import Path
 from psengine.config import Config, get_config
 
-Config.init(config_path="config.toml")
+Config.init(
+    config_path=Path(__file__).parent / "config.toml"
+)
 config = get_config()
 print(config.my_value)
