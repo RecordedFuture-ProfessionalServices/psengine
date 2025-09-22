d4160f73c96bfb9fa1d660a274d26fedb81285e7
mmedici <moise.medici@recordedfuture.com>
2025-09-22 17:04:31 +0100

    path

--- docs/examples/config/example_1.py
+++ docs/examples/config/example_1.py
@@ -1,8 +1,9 @@
 from pathlib import Path
 
-from psengine.config import get_config
+from psengine.config import get_config, Config
 
-CONFIG_PATH = Path.cwd() / 'config.toml'
+CONFIG_PATH = Path(__file__).parent / 'config.toml'
 
+Config.init(config_path=CONFIG_PATH)
 config = get_config()
 print(config.my_value)
