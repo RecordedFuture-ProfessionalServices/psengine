caa2432f13172ee4eb5abe805eb7fbc792dba65b
mmedici <moise.medici@recordedfuture.com>
2025-09-24 10:15:49 +0100

    fusion docs examples

---
+++ docs/examples/fusion/example_1.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+
+from psengine.fusion import FusionMgr
+
+file_to_upload = Path(__file__).parent / 'file.txt'
+file_to_upload.touch(exist_ok=True)
+file_to_upload.write_text('example')
+
+
+mgr = FusionMgr()
+post_result = mgr.post_file(file_to_upload, '/home/file.txt')
+
+print(post_result)
