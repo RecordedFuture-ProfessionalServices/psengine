64d0e3aadf7db3d91e8c5b8e464f1b521120f47a
ebartosevic <ernest.bartosevic@recordedfuture.com>
2025-10-08 13:13:46 +0100

    ruff format

--- docs/examples/enrich/example_5.py
+++ docs/examples/enrich/example_5.py
@@ -2,6 +2,9 @@ from psengine.enrich import LookupMgr
 
 mgr = LookupMgr()
 ip = mgr.lookup('45.83.236.105', 'ip', fields=['links'])
-malwares = ip.links(from_section='Actors, Tools & TTPs', entity_type='Malware')
+malwares = ip.links(
+    from_section='Actors, Tools & TTPs',
+    entity_type='Malware',
+)
 
 print(', '.join(malwares))
