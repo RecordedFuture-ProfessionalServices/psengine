92d0383d4b892294d5471e23f626a7751d4633e2
ebartosevic <ernest.bartosevic@recordedfuture.com>
2026-01-21 15:54:58 +0200

    PSF-1180 minimal dev token changes

--- psengine/constants.py
+++ psengine/constants.py
@@ -23,7 +23,7 @@ DEFAULT_MAX_WORKERS = 10
 # Recorded Future API
 #####################
 RF_TOKEN_ENV_VAR = 'RF_TOKEN'  # noqa: S105
-RF_TOKEN_VALIDATION_REGEX = r'^[a-f0-9]{32}$'  # noqa: S105
+RF_TOKEN_VALIDATION_REGEX = r'^([a-f0-9]{32}|[a-z]{32})$'  # noqa: S105
 
 #####################
 # Recorded Future Portal
